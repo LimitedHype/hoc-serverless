993c4835d225e8c913d1097c9070f7788673d47c
Charlie Deane <charlesdeane9@gmail.com>
2020-07-01 01:56:15 +0000

    Initial commit

---
+++ api/execute.js
@@ -0,0 +1,37 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+export default async (req, res) => {
+    try {
+        const {
+            secret,
+            email,
+            ip,
+            cards
+        } = req.body;
+        if (secret != process.env.SECRET)
+            return res.send(403).json({
+                success: false
+            });
+        await stripe.radar.valueListItems.create({
+            value_list: process.env.STRIPE_EMAIL_LIST,
+            value: email,
+        })
+        await stripe.radar.valueListItems.create({
+            value_list: process.env.STRIPE_IP_LIST,
+            value: ip,
+        })
+        cards.forEach(async (card) => {
+            await stripe.radar.valueListItems.create({
+                value_list: process.env.STRIPE_CARD_FINGERPRINT_LIST,
+                value: card,
+            })
+        });
+        return res.send(200).json({
+            success: true
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false
+        });
+    }
+}
